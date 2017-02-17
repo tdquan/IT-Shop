67624efa01d921e957f05175595bdd6b9c98aa73
Quan Trinh <trinhquan1123@yahoo.com>
2017-02-17 14:06:10 +0100

    able to add basic package

--- app/controllers/spree/packages_controller.rb
+++ app/controllers/spree/packages_controller.rb
@@ -10,14 +10,25 @@ class Spree::PackagesController < Spree::StoreController
   end
 
   def add
-    redirect_to root_path
+    # Define the taxon that include all the package items
     @taxon = @package.taxons.first
     return unless @taxon
 
+    # Find all products in the package
     @searcher = build_searcher(params.merge(taxon: @taxon.id, include_images: true))
     @products = @searcher.retrieve_products
     @taxonomies = Spree::Taxonomy.includes(root: :children)
-    raise
+
+    # Add products into the cart as line items
+    @products.each do |p|
+      order = current_spree_user.orders.where(state: "cart").last
+      variant = Spree::Variant.where(product_id: p.id).first
+      item = order.line_items.new(variant_id: variant.id)
+      item.quantity = params[:package][:quantity]
+      item.save
+    end
+
+    redirect_to root_path
   end
 
   private
