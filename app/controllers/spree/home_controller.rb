5eeeeacc51ffce9ec70108db38cbe1c9ab71cc60
Quan Trinh <trinhquan1123@yahoo.com>
2017-02-20 12:03:51 +0100

    able to correctly added default items for each package to cart

--- app/controllers/spree/home_controller.rb
+++ app/controllers/spree/home_controller.rb
@@ -1,11 +1,9 @@
 class Spree::HomeController < ApplicationController
   def index
     @taxonomies = Spree::Taxonomy.includes(root: :children)
-    @features_1 = Spree::Taxonomy.all[0].taxons[0].products
-    @features_2 = Spree::Taxonomy.all[1].taxons[0].products
-    @features_3 = Spree::Taxonomy.all[2].taxons[0].products
-    if current_spree_user.orders.last.state == "cart"
-      @order = current_spree_user.orders.last
+
+    if !current_spree_user.orders.where(state: "cart").last.nil?
+      @order = current_spree_user.orders.where(state: "cart").last
     else
       @order = current_spree_user.orders.new
     end
--- app/controllers/spree/packages_controller.rb
+++ app/controllers/spree/packages_controller.rb
@@ -2,30 +2,35 @@ class Spree::PackagesController < Spree::StoreController
   before_action :set_package
 
   def show
-    if current_spree_user.orders.last.state == "cart"
-      @order = current_spree_user.orders.last
+    if !current_spree_user.orders.where(state: "cart").last.nil?
+      @order = current_spree_user.orders.where(state: "cart").last
     else
       @order = current_spree_user.orders.new
     end
   end
 
   def add
+    if !current_spree_user.orders.where(state: "cart").last.nil?
+      @order = current_spree_user.orders.where(state: "cart").last
+    else
+      @order = current_spree_user.orders.create
+    end
+
     # Define the taxon that include all the package items
     @taxon = @package.taxons.first
     return unless @taxon
 
     # Find all products in the package
     @searcher = build_searcher(params.merge(taxon: @taxon.id, include_images: true))
-    @products = @searcher.retrieve_products
+    @products = @searcher.retrieve_products.where("meta_keywords like?", "%#{@package.name.downcase}_default%")
     @taxonomies = Spree::Taxonomy.includes(root: :children)
 
     # Add products into the cart as line items
     @products.each do |p|
-      order = current_spree_user.orders.where(state: "cart").last
       variant = Spree::Variant.where(product_id: p.id).first
-      item = order.line_items.new(variant_id: variant.id)
+      item = @order.line_items.new(variant_id: variant.id)
       item.quantity = params[:package][:quantity]
-      order.contents.update_cart({}) # optional: set parameter here to be order_params and un-comment the order_params section
+      @order.contents.update_cart({}) # optional: set parameter here to be order_params and un-comment the order_params
       item.save
     end
 
