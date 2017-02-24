16bd1b87de79f5cb0228a6da3425afd68b1f82cc
Quan Trinh <trinhquan1123@yahoo.com>
2017-02-24 15:48:11 +0100

    some responsive fix

--- app/assets/javascripts/jquery_readyselector.js
+++ app/assets/javascripts/jquery_readyselector.js
@@ -1,19 +1,34 @@
 $(document).on('turbolinks:load', function() {
   $('.home.show').ready(function() {
     $('.package-selection').css({'margin-bottom': $('.package-details').height()});
-  });
 
-  $('.home.index').ready(function() {
+    // Equal heights for all features columns
     var max_height = 0;
 
-    $('.category-desctiption').each(function() {
+    $('.package-overview-details').each(function() {
       if ($(this).height() > max_height) {
         max_height = $(this).height();
       }
       return max_height;
     });
 
-    $('.category-desctiption').each(function() {
+    $('.package-overview-details').each(function() {
+      $(this).height(max_height);
+    });
+  });
+
+  $('.home.index').ready(function() {
+    // Equal heights for all descriptions columns
+    var max_height = 0;
+
+    $('.category-description').each(function() {
+      if ($(this).height() > max_height) {
+        max_height = $(this).height();
+      }
+      return max_height;
+    });
+
+    $('.category-description').each(function() {
       $(this).height(max_height);
     });
   });
