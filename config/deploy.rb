3ac971061d023fe74fa790f3e5045493f9f77d2f
Quan Trinh <trinhquan1123@yahoo.com>
2017-03-01 14:10:14 +0100

    setting up capistrano again

--- config/deploy.rb
+++ config/deploy.rb
@@ -49,6 +49,10 @@ set :puma_preload_app, true
 set :puma_worker_timeout, nil
 set :puma_init_active_record, true  # Change to false when not using ActiveRecord
 
+# Linked Files & Directories (Default None):
+# set :linked_files, %w{config/database.yml}
+set :linked_dirs,  %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+
 namespace :puma do
   desc 'Create Directories for Puma Pids and Socket'
   task :make_dirs do
