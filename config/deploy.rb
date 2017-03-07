63f7137ae83ad3c4f2bbfc389a64ac5f390e19a3
lucien mollard <mollard.lucien@gmail.com>
2017-03-07 10:07:50 +0100

    unicorn config + deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,8 +1,8 @@
 # config valid only for current version of Capistrano
 lock "3.7.2"
 
-set :application, "my_app_name"
-set :repo_url, "git@example.com:me/my_repo.git"
+set :application, "shop-it"
+set :repo_url, "git@git.cbm-groupe.fr:tdquan/shop-it.git"
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
