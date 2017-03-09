1995c046b72e48be0af806acc83bb75eba677eba
Quan Trinh <trinhquan1123@yahoo.com>
2017-03-09 15:42:42 +0100

    setup rbenv capistrano

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -26,7 +26,6 @@ Rails.application.configure do
   config.serve_static_assets = true
   config.assets.compile = true
   config.assets.digest = true
-  config.assets.precompile = false
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
