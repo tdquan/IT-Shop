228d0dc91566f6a8e26527ac4cdc2f0c531ccaa7
Romain Bousseau <romainbousseau1@gmail.com>
2017-03-31 13:43:54 +0200

    clean files

--- app/assets/images/products/Learn JavaScript _ Codecademy_files/main.js
+++
@@ -1,9 +0,0 @@
-function main (){
- $('.skillset').hide();
- $('.skillset').fadeIn(1000);
- $('.projects').hide();
- $('.projects-button').on('click', function() {
-   $('.projects').toggle();
- });
-}
-$(document).ready(main);
